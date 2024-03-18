3767f91d33b0bf1f850995fbc63e961fa816885d
Ajju-kld <mohammedajmal996@gmail.com>
2024-03-18 07:21:10 +0530

    Add courses app and update settings and URLs

---
+++ courses/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Course(models.Model):
+    course_id = models.CharField(max_length=10)
+    dept = models.CharField(max_length=50)
+    no_of_modules = models.IntegerField()
+    semester = models.IntegerField()
+    scheme = models.CharField(max_length=20)
+    course_name = models.CharField(max_length=100)
+    credits = models.IntegerField()
+    hours = models.IntegerField()
+    
+
+class Topic(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    topic_name = models.CharField(max_length=100)
+    max_time = models.IntegerField()
+    priority = models.IntegerField()
+    focus_area = models.BooleanField()
+
+    
